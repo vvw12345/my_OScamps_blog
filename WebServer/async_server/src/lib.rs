8e0bedd5da32a3e6d987b7d513c58d44af6bb4fb
root <root@LAPTOP-E3UTKH7E>
2024-05-28 09:18:35 +0800

    代码注释

--- WebServer/async_server/src/lib.rs
+++ WebServer/async_server/src/lib.rs
@@ -5,8 +5,11 @@ use std::{
 #[derive(Default)]
 pub struct Parker(Mutex<bool>, Condvar);
 
+// 实现线程的暂停和唤醒
 impl Parker {
+    // 将当前线程暂停 直到被唤醒
     fn park(&self) {
+        // 获得死锁并取出值   
         let mut resumable = self.0.lock().unwrap();
         while !*resumable {
             resumable = self.1.wait(resumable).unwrap();
@@ -14,12 +17,15 @@ impl Parker {
         *resumable = false;
     }
 
+    // 唤醒一个之前被暂停的线程
     fn unpark(&self) {
         *self.0.lock().unwrap() = true;
+        // 用信号量机制唤醒一个被阻塞的线程
         self.1.notify_one();
     }
 }
 
+// 阻塞等待一个Future直到其完成
 pub fn block_on<F: Future>(mut future: F) -> F::Output {
     let parker = Arc::new(Parker::default());
     let mywaker = Arc::new(MyWaker { parker: parker.clone() });
@@ -35,21 +41,25 @@ pub fn block_on<F: Future>(mut future: F) -> F::Output {
     }
 }
 
+// 自定义的waker结构体，用于和Parker一起工作
 pub struct MyWaker {
     parker: Arc<Parker>,
 }
 
+// 唤醒 MyWaker，解除相关线程的暂停状态。
 pub fn mywaker_wake(s: &MyWaker) {
     let waker_arc = unsafe { Arc::from_raw(s) };
     waker_arc.parker.unpark();
 }
 
+// 克隆 MyWaker 的函数。
 pub fn mywaker_clone(s: &MyWaker) -> RawWaker {
     let arc = unsafe { Arc::from_raw(s) };
     std::mem::forget(arc.clone());
     RawWaker::new(Arc::into_raw(arc) as *const (), &VTABLE)
 }
 
+// 定义 RawWaker 的行为的 RawWakerVTable。
 const VTABLE: RawWakerVTable = RawWakerVTable::new(
     |s| mywaker_clone(unsafe { &*(s as *const MyWaker) }),
     |s| mywaker_wake(unsafe { &*(s as *const MyWaker) }),
@@ -57,6 +67,7 @@ const VTABLE: RawWakerVTable = RawWakerVTable::new(
     |s| drop(unsafe { Arc::from_raw(s as *const MyWaker) }),
 );
 
+// 将 MyWaker 转换为 Waker 的函数。
 pub fn mywaker_into_waker(s: *const MyWaker) -> Waker {
     let raw_waker = RawWaker::new(s as *const (), &VTABLE);
     unsafe { Waker::from_raw(raw_waker) }
@@ -75,8 +86,10 @@ impl Task {
     }
 }
 
+// 为 Task 实现 Future trait。
 impl Future for Task {
     type Output = usize;
+    // poll 方法用于检查任务是否准备就绪。
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut r = self.reactor.lock().unwrap();
         if r.is_ready(self.id) {
@@ -92,24 +105,28 @@ impl Future for Task {
     }
 }
 
+// 枚举，表示任务的状态。
 pub enum TaskState {
     Ready,
     NotReady(Waker),
     Finished,
 }
 
+// Reactor 结构体，用于管理和分发事件。
 pub struct Reactor {
     dispatcher: Sender<Event>,
     handle: Option<thread::JoinHandle<()>>,
     tasks: HashMap<usize, TaskState>,
 }
 
+// 枚举，表示不同类型的事件。
 pub enum Event {
     Close,
     Timeout(u64, usize),
 }
 
 impl Reactor {
+    // 创建一个新的 Reactor 实例。
     pub fn new() -> Arc<Mutex<Self>> {
         let (tx, rx) = mpsc::channel::<Event>();
         let reactor = Arc::new(Mutex::new(Reactor {
@@ -141,6 +158,7 @@ impl Reactor {
         reactor
     }
 
+    // 唤醒一个指定 id 的任务。
     fn wake(&mut self, id: usize) {
         let state = self.tasks.get_mut(&id).unwrap();
         match std::mem::replace(state, TaskState::Ready) {
@@ -150,6 +168,7 @@ impl Reactor {
         }
     }
 
+    // 注册一个新的任务，包括其持续时间和 waker。
     fn register(&mut self, duration: u64, waker: Waker, id: usize) {
         if self.tasks.insert(id, TaskState::NotReady(waker)).is_some() {
             panic!("Tried to insert a task with id: '{}', twice!", id);
@@ -157,10 +176,12 @@ impl Reactor {
         self.dispatcher.send(Event::Timeout(duration, id)).unwrap();
     }
 
+    // 关闭Reactor
     pub fn close(&mut self) {
         self.dispatcher.send(Event::Close).unwrap();
     }
 
+    // 检查一个任务是否已经准备就绪。
     fn is_ready(&self, id: usize) -> bool {
         self.tasks.get(&id).map(|state| match state {
             TaskState::Ready => true,
@@ -169,6 +190,7 @@ impl Reactor {
     }
 }
 
+// 为 Reactor 实现 Drop trait，确保正确清理。
 impl Drop for Reactor {
     fn drop(&mut self) {
         self.handle.take().map(|h| h.join().unwrap()).unwrap();
--- WebServer/async_server/src/main.rs
+++ WebServer/async_server/src/main.rs
@@ -11,26 +11,33 @@ fn main() {
     let start = Instant::now();
     let reactor = Reactor::new();
 
+    // 绑定8080端口 监听连接
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    // 用迭代器取出连接尝试（不一定成功）
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        // Arc复制一份Reactor 
         let reactor_clone = reactor.clone();
         
+        // 异步执行
         let fut = async move {
             handle_connection(stream, reactor_clone).await;
             println!("Connection handled at time: {:.2}.", start.elapsed().as_secs_f32());
         };
-
         block_on(fut);
     }
 
     reactor.lock().map(|mut r| r.close()).unwrap();
 }
 
+// 消息处理函数
 async fn handle_connection(mut stream: TcpStream, reactor: Arc<Mutex<Reactor>>) {
+    // 创建缓冲区 用于从流中读取数据
     let buf_reader = BufReader::new(&mut stream);
+    // 从流中读出请求行
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
+    // 根据请求行确定请求状态和请求的路径
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
